5bb0082444f5e580cde04a1364ad840913dc6508
Azzorg <c.goupil72@gmail.com>
2017-11-24 15:08:31 -0500

    Entête : typescript => récupération du panier pour avoir le nombre d'item

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 /**
  * Defines the main component of the application.
@@ -9,11 +10,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  // TODO: Modifier le nom des auteurs pour vos noms
+  /** Variables */
+  getPanierRequest: string = 'http://localhost:3000/api/shopping-cart/';
+  nbItem: number = 0;
+  items = new Array();
+
+  constructor(private http: HttpClient) {
+    console.log("constructor");
+  }
+
   readonly authors = [
-    'Antoine Béland',
-    'Konstantinos Lambrou-Latreille'
+    'Camille Goupil (1922286)',
+    'Théo Charlot (1922297)'
   ];
 
-  // TODO: À compléter
+  /**
+   * Occurs when the component is initialized.
+   */
+  ngOnInit() {
+    this.getNbItemCart();
+  }
+
+
+  /** 
+   * Récupération du panier pour avoir le nombre d'item qui sont présents dedans 
+   */
+  getNbItemCart(): void {
+    this.http.get(this.getPanierRequest).subscribe(
+      data => {
+        console.log(data);
+        this.items = data as Object[];
+        this.nbItem = this.items.length;
+        console.log("Nombre d'item dans le panier : " + this.items.length)
+      },
+      err => {
+        console.error("Erreur : " + err);
+      });
+  }
 }
