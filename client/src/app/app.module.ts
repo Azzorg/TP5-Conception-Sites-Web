af5c2bf5871970d8eb3c9d286cc08270c7253cf5
Azzorg <c.goupil72@gmail.com>
2017-11-26 15:25:44 -0500

    App-module => ajout du service order

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { ConfirmationComponent } from './confirmation/confirmation.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductsService } from './products.service';
 import { ShoppingCartService } from './shopping-cart.service';
+import { OrderService } from './order.service';
 
 // Application routes
 const appRoutes: Routes = [
@@ -54,7 +55,8 @@ const appRoutes: Routes = [
   ],
   providers: [
     ProductsService,
-    ShoppingCartService
+    ShoppingCartService,
+    OrderService
   ],
   bootstrap: [AppComponent]
 })
