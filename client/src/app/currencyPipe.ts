33965c71f03cd2edfe5100fc7addf26134d88511
Azzorg <c.goupil72@gmail.com>
2017-12-01 17:17:16 -0500

    Pipe : currency => ajout dans le pipe de la monnaie (ici $)

--- client/src/app/currencyPipe.ts
+++ client/src/app/currencyPipe.ts
@@ -7,11 +7,12 @@ export class CurrencyFormat {
     transform(value: number,
         decimalLength: number = 2,
         decimalDelimiter: string = ',',
+        currency : string = " $",
         chunkLength: number = 3): string {
 
         let result = '\\d(?=(\\d{' + chunkLength + '})+' + (decimalLength > 0 ? '\\D' : '$') + ')'
         let num = value.toFixed(Math.max(0, ~~decimalLength));
 
-        return (decimalDelimiter ? num.replace('.', decimalDelimiter) : num).replace(new RegExp(result, 'g'), '$&');
+        return (decimalDelimiter ? num.replace('.', decimalDelimiter) : num).replace(new RegExp(result, 'g'), '$&') + currency;
     }
 }
