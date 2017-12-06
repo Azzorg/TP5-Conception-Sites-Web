8b2c50cb17ae3be22885726eebdcdbb64a58b762
Azzorg <c.goupil72@gmail.com>
2017-12-06 17:04:23 -0500

    Confirmation : TS => récupération des informations de la commande suivant l'id d'order reçu en paramètre

--- client/src/app/confirmation/confirmation.component.ts
+++ client/src/app/confirmation/confirmation.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OrderService } from '../order.service';
 
 /**
 * Defines the component responsible to manage the confirmation page.
@@ -8,5 +10,28 @@ import { Component } from '@angular/core';
   templateUrl: './confirmation.component.html'
 })
 export class ConfirmationComponent {
-  // TODO: À compléter
+
+  /* Variables */
+  numOrder: number;
+  userName: string;
+
+  constructor(private orderService: OrderService, private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.numOrder = +params['id'];
+      console.log("numéro de commande : " + this.numOrder);
+      this.getOrder();
+    });
+  }
+
+  getOrder() {
+    this.orderService.getOrder(this.numOrder)
+    .then(value => {
+      console.log("COMMANDE :" );
+      console.log(value);
+      this.userName = value.firstName + " " + value.lastName;
+    });
+  }
 }
