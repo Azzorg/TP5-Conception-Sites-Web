45b3286b305f58de3bb652874ece0725fbdb21eb
Azzorg <c.goupil72@gmail.com>
2017-12-06 19:40:00 -0500

    Product => utilisation de hidden + submit au llieu de click

--- client/src/app/product/product.component.ts
+++ client/src/app/product/product.component.ts
@@ -17,17 +17,18 @@ import { CurrencyFormat } from './../currencyPipe';
 })
 export class ProductComponent implements OnInit {
 
-  product : Product = new Product();
-  quantity : number;
-  id : number;
-  isAlreadyInCart : boolean = false;
+  product: Product = new Product();
+  quantity: number;
+  id: number;
+  isAlreadyInCart: boolean = false;
+  public saved: boolean = false;
 
   /**
    * Initializes a new instance of the ProductComponent class.
    *
    * @param route                   The active route.
    */
-  constructor(private route: ActivatedRoute, private productsService : ProductsService, private shoppingCartService : ShoppingCartService, public router : Router) { }
+  constructor(private route: ActivatedRoute, private productsService: ProductsService, private shoppingCartService: ShoppingCartService, public router: Router) { }
 
   /**
    * Occurs when the component is initialized.
@@ -38,37 +39,52 @@ export class ProductComponent implements OnInit {
     //get the product via the rest request
     this.productsService.getProduct(this.id)
       .then(value => {
-        this.product = value;
-        console.log("ID PRODUCT : " + this.id);
-        this.shoppingCartService.getItem(this.id)
-          .then(value => {
-            this.isAlreadyInCart = true;
-            this.quantity = value.quantity;
-            console.log("quantity : " + value.quantity);
-          })
-          .catch(value => {
-            this.isAlreadyInCart = false;
-            this.quantity = 1;
-          });
+        if (value) {
+          console.log(" ////////////VALUE");
+          console.log(value);
+          this.product = value;
+          console.log("ID PRODUCT : " + this.id);
+          this.shoppingCartService.getItem(this.id)
+            .then(value => {
+              this.isAlreadyInCart = true;
+              this.quantity = value.quantity;
+              console.log("quantity : " + value.quantity);
+            })
+            .catch(value => {
+              this.isAlreadyInCart = false;
+              this.quantity = 1;
+            });
+        }
+        else {
+          console.log("ERREUR");
+          //product not existing => error page
+          this.router.navigate(["/\*\*"]);
+        }
       })
       .catch(value => {
+        console.log("ERREUR");
         //product not existing => error page
         this.router.navigate(["/\*\*"]);
       });
   }
 
 
-  addProduct(){
+  addProduct() {
     let prod: ProductItem = new ProductItem();
     prod.productId = this.product.id;
     prod.quantity = this.quantity;
-    if(this.isAlreadyInCart){
+    if (this.isAlreadyInCart) {
       this.shoppingCartService.putItem(prod);
     }
     else {
       this.shoppingCartService.postItem(prod);
       this.isAlreadyInCart = true;
     }
+    this.saved = true;
+    setTimeout(function () {
+      this.saved = false;
+      console.log(this.saved);
+    }.bind(this), 5000);
 
   }
 }
